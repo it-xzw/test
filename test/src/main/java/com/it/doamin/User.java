6a51ac9dc53885766955d2a5b2fec76db67d9be1
username <2267455901@qq.com>
2019-07-11 09:06:20 +0800

    change

---
+++ test/src/main/java/com/it/doamin/User.java
@@ -0,0 +1,11 @@
+package com.it.doamin;
+
+/**
+ * @auther admin
+ * @date 2019/7/10 21:40
+ * @description $DESCRIPTION
+ */
+public class User {
+    private String username;
+    private Integer age;
+}
